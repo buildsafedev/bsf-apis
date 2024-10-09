cfc1792add1764ae3de035854628b59047cd5fa9
House <house@buildsafe.dev>
2024-10-09 22:57:51 +0530

    Generate JS/grpc-web client
    Signed-off-by: House <house@buildsafe.dev>

--- bsf/flake.nix
+++ bsf/flake.nix
@@ -3,10 +3,10 @@
 	description = "";
 	
 	inputs = {
-		 nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746.url = "github:nixos/nixpkgs/a731d0cb71c58f56895f71a5b02eda2962a46746";
-		 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7.url = "github:nixos/nixpkgs/1ebb7d7bba2953a4223956cfb5f068b0095f84a7";
 		 nixpkgs-6d974716629d3bf8a31b519952c6a5754a41bd38.url = "github:nixos/nixpkgs/6d974716629d3bf8a31b519952c6a5754a41bd38";
 		 nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14.url = "github:nixos/nixpkgs/ac5c1886fd9fe49748d7ab80accc4c847481df14";
+		 nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746.url = "github:nixos/nixpkgs/a731d0cb71c58f56895f71a5b02eda2962a46746";
+		 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7.url = "github:nixos/nixpkgs/1ebb7d7bba2953a4223956cfb5f068b0095f84a7";
 			
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 		 gomod2nix.url = "github:nix-community/gomod2nix";
@@ -27,10 +27,10 @@
 	
 	
 	
-	 nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746, 
-	 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7, 
 	 nixpkgs-6d974716629d3bf8a31b519952c6a5754a41bd38, 
 	 nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14, 
+	 nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746, 
+	 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7, 
 	 }: let
 	  supportedSystems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
 	  
@@ -38,10 +38,10 @@
 	  forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
 		inherit system;
 		
-		 nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs = import nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746 { inherit system; };
-		 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs = import nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7 { inherit system; };
 		 nixpkgs-6d974716629d3bf8a31b519952c6a5754a41bd38-pkgs = import nixpkgs-6d974716629d3bf8a31b519952c6a5754a41bd38 { inherit system; };
 		 nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs = import nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14 { inherit system; };
+		 nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs = import nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746 { inherit system; };
+		 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs = import nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7 { inherit system; };
 		
 		 buildGoApplication = gomod2nix.legacyPackages.${system}.buildGoApplication;
 		pkgs = import nixpkgs { inherit system;  };
@@ -54,10 +54,10 @@
 		 buildGoApplication, 
 		
 		
-		 nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs, 
-		 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs, 
 		 nixpkgs-6d974716629d3bf8a31b519952c6a5754a41bd38-pkgs, 
 		 nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs, 
+		 nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs, 
+		 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs, 
 		 ... }: {
 		default = pkgs.callPackage ./default.nix {
 			 inherit buildGoApplication;
@@ -72,10 +72,10 @@
 		 buildGoApplication, 
 		
 		
-		 nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs, 
-		 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs, 
 		 nixpkgs-6d974716629d3bf8a31b519952c6a5754a41bd38-pkgs, 
 		 nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs, 
+		 nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs, 
+		 nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs, 
 		 ... }: {
 		devShell = pkgs.mkShell {
 		  # The Nix packages provided in the environment
@@ -94,7 +94,7 @@
 		 buildGoApplication, 
 		
 		
-		 nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs,  nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs,  nixpkgs-6d974716629d3bf8a31b519952c6a5754a41bd38-pkgs,  nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs,  ... }: {
+		 nixpkgs-6d974716629d3bf8a31b519952c6a5754a41bd38-pkgs,  nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs,  nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs,  nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs,  ... }: {
 		runtime = pkgs.buildEnv {
 		  name = "runtimeenv";
 		  paths = [ 
@@ -108,7 +108,7 @@
 		 buildGoApplication, 
 		
 		
-	    nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs,  nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs,  nixpkgs-6d974716629d3bf8a31b519952c6a5754a41bd38-pkgs,  nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs,  ... }: {
+	    nixpkgs-6d974716629d3bf8a31b519952c6a5754a41bd38-pkgs,  nixpkgs-ac5c1886fd9fe49748d7ab80accc4c847481df14-pkgs,  nixpkgs-a731d0cb71c58f56895f71a5b02eda2962a46746-pkgs,  nixpkgs-1ebb7d7bba2953a4223956cfb5f068b0095f84a7-pkgs,  ... }: {
 		development = pkgs.buildEnv {
 		  name = "devenv";
 		  paths = [ 
---
+++ js/provider.js
@@ -0,0 +1,23 @@
+// source: buildsafe/v1/auth.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+goog.provide('proto.buildsafe.v1.Provider');
+
+/**
+ * @enum {number}
+ */
+proto.buildsafe.v1.Provider = {
+  PROVIDER_UNSPECIFIED: 0,
+  PROVIDER_GITHUB: 1
+};
+
