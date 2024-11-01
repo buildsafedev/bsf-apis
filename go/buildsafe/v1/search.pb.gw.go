b25f7db3c3152dcdc9e4ea3870e61e3d20a0e9c1
Dipankar Das <65275144+dipankardas011@users.noreply.github.com>
2024-11-01 23:59:22 +0530

    chore(generated): client from grpc for projects service
    Signed-off-by: Dipankar Das <65275144+dipankardas011@users.noreply.github.com>

--- go/buildsafe/v1/search.pb.gw.go
+++ go/buildsafe/v1/search.pb.gw.go
@@ -35,11 +35,7 @@ func request_SearchService_ListPackages_0(ctx context.Context, marshaler runtime
 	var protoReq ListPackagesRequest
 	var metadata runtime.ServerMetadata
 
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -52,11 +48,7 @@ func local_request_SearchService_ListPackages_0(ctx context.Context, marshaler r
 	var protoReq ListPackagesRequest
 	var metadata runtime.ServerMetadata
 
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -337,6 +329,7 @@ func local_request_SearchService_FetchMaintainersInfo_0(ctx context.Context, mar
 // UnaryRPC     :call SearchServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
 // Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterSearchServiceHandlerFromEndpoint instead.
+// GRPC interceptors will not work for this type of registration. To use interceptors, you must use the "runtime.WithMiddlewares" option in the "runtime.NewServeMux" call.
 func RegisterSearchServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux, server SearchServiceServer) error {
 
 	mux.Handle("POST", pattern_SearchService_ListPackages_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
@@ -470,21 +463,21 @@ func RegisterSearchServiceHandlerServer(ctx context.Context, mux *runtime.ServeM
 // RegisterSearchServiceHandlerFromEndpoint is same as RegisterSearchServiceHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterSearchServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
-	conn, err := grpc.Dial(endpoint, opts...)
+	conn, err := grpc.NewClient(endpoint, opts...)
 	if err != nil {
 		return err
 	}
 	defer func() {
 		if err != nil {
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Errorf("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 			return
 		}
 		go func() {
 			<-ctx.Done()
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Errorf("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 		}()
 	}()
@@ -502,7 +495,7 @@ func RegisterSearchServiceHandler(ctx context.Context, mux *runtime.ServeMux, co
 // to "mux". The handlers forward requests to the grpc endpoint over the given implementation of "SearchServiceClient".
 // Note: the gRPC framework executes interceptors within the gRPC handler. If the passed in "SearchServiceClient"
 // doesn't go through the normal gRPC flow (creating a gRPC client etc.) then it will be up to the passed in
-// "SearchServiceClient" to call the correct interceptors.
+// "SearchServiceClient" to call the correct interceptors. This client ignores the HTTP middlewares.
 func RegisterSearchServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux, client SearchServiceClient) error {
 
 	mux.Handle("POST", pattern_SearchService_ListPackages_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
